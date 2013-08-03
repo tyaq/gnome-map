2463d1f92db7bdde741ce7211b07f4f48407cd08
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-08-03 16:34:24 -0400

    io

--- CS 2110 Final (Road Map)/src/roadMapGUI/GUI.java
+++ CS 2110 Final (Road Map)/src/roadMapGUI/GUI.java
@@ -21,6 +21,16 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JMenu;
 
@@ -56,5 +66,62 @@ public class GUI extends JFrame{
 	    frame.setVisible(true);
 	}//End main method
 	
+	//For working on existing catalog
+	/**
+	 * 
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public void importCountry() throws IOException, ClassNotFoundException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		//Ask user for either a file path of existing library
+					System.out.println("Give the path of a file to import your library or leave it empty.");
+					String inputFilePath=br.readLine();
+					
+				//Checks if file exists	
+					File file = new File(inputFilePath);
+					if (file.exists() && file.canRead()) {
+							FileInputStream fis = new FileInputStream(inputFilePath);
+					        ObjectInputStream ois = new ObjectInputStream(fis);
+					        
+					        g = (Graph) (ois.readObject());
+					        pg=(PaintGraph) (ois.readObject());
+					        menu =(MenuBar) ois.readObject();
+					        fis.close();
+					        System.out.println("Your library has been loaded.\n");
+						}
+		
+	}
+	
+	//For saving out catalog
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public void exportCountry() throws IOException {
+	InputStreamReader isr = new InputStreamReader(System.in);
+	BufferedReader br = new BufferedReader(isr);
+			System.out.println("Write File path to save your library.");
+			String outputFileName=br.readLine();
+			File file = new File(outputFileName);
+			file.createNewFile();
+			FileOutputStream fos = null;
+		    ObjectOutputStream oos = null;
+				
+				try {
+			        fos = new FileOutputStream(outputFileName,false);
+			        oos = new ObjectOutputStream(fos);
+			        oos.writeObject(g);
+			        oos.writeObject(pg);
+			        oos.writeObject(menu);
+			        oos.close();
+			        System.out.println("Library saved. Thank you for using Omish Music Cataloger");
+			    } catch (IOException ex) {
+			        ex.printStackTrace();
+			    }
+				System.out.println("You can find it encryped at " + outputFileName + ". You will need it to import your library later.");
+		
+	}
 
 }//End Class
